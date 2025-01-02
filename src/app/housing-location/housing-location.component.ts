3b3236defca71127f61918260fff35357f47deab
Ankit Singh <ankitsingh635101@gmail.com>
2025-01-02 23:03:44 +0530

    refactor: enhance HousingLocationComponent with Input property and CommonModule import

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,15 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocation } from "../housinglocation";
 
 @Component({
-  selector: 'app-housing-location',
-  imports: [],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-  styles: ``
+  selector: "app-housing-location",
+  imports: [CommonModule],
+  template: ` <p>housing-location works!</p> `,
+  styles: ``,
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
