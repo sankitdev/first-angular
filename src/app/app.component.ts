658b91cc7fb787f14c067112c106e98a22f1ed08
Ankit Singh <ankitsingh635101@gmail.com>
2025-01-03 12:43:32 +0530

    feat: add routing with DetailsComponent and update AppComponent for router integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
+import { RouterModule } from "@angular/router";
 @Component({
   selector: "app-root",
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
     <main>
       <header class="brand-name">
@@ -14,7 +15,7 @@ import { HomeComponent } from "./home/home.component";
         />
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
