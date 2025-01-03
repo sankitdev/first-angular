669e11c85f814d3bf21c1c8f6bf61066265757cd
Ankit Singh <ankitsingh635101@gmail.com>
2025-01-03 13:33:03 +0530

    feat: enhance DetailsComponent with dynamic data display and routing integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,19 @@ import { HomeComponent } from "./home/home.component";
 import { RouterModule } from "@angular/router";
 @Component({
   selector: "app-root",
-  imports: [HomeComponent, RouterModule],
+  imports: [RouterModule],
   template: `
     <main>
-      <header class="brand-name">
-        <img
-          class="brand-logo"
-          src="/assets/logo.svg"
-          alt="logo"
-          aria-hidden="true"
-        />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img
+            class="brand-logo"
+            src="/assets/logo.svg"
+            alt="logo"
+            aria-hidden="true"
+          />
+        </header>
+      </a>
       <section class="content">
         <router-outlet></router-outlet>
       </section>
